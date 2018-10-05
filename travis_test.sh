4aeb26067290980ac9eb2069790c6cda7c532889
Jim Chiang <jchiang@slac.stanford.edu>
2018-10-05 15:04:28 -0700

    clone imSim and obs_lsstCam since they are now dependencies

--- travis_test.sh
+++ travis_test.sh
@@ -4,8 +4,19 @@ source loadLSST.bash
 setup lsst_sims
 pip install nose
 pip install coveralls
+git clone https://github.com/lsst/obs_lsstCam.git
+cd obs_lsstCam
+git checkout u/jchiang/imsim_crosstalk
+setup -r . -j
+scons
+cd ..
+git clone https://github.com/LSSTDESC/imSim.git
+cd imSim
+setup -r . -j
+scons
+cd ..
 eups declare desc_sim_utils -r ${TRAVIS_BUILD_DIR} -t current
 setup desc_sim_utils
 cd ${TRAVIS_BUILD_DIR}
-scons opt=3
+scons
 nosetests -s --with-coverage --cover-package=desc.sim_utils
