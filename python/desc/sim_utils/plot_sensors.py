9b304e9503f4a29be7c0aafc4a8cb29cbee86375
Jim Chiang <jchiang@slac.stanford.edu>
2019-01-06 10:23:20 -0800

    enable clients to set individual patch colors

--- python/desc/sim_utils/plot_sensors.py
+++ python/desc/sim_utils/plot_sensors.py
@@ -10,7 +10,6 @@ import lsst.afw.geom as afw_geom
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
     import lsst.sims.coordUtils
-from .trim_sensors import get_obs_md
 
 __all__ = ['make_patch', 'plot_sensors', 'plot_skymap_tract', 'set_xylims']
 
@@ -91,7 +90,8 @@ def plot_sensors(sensors, camera, obs_md, ax=None, color='red',
     return ax
 
 
-def plot_skymap_tract(skyMap, tract=0, title=None, ax=None):
+def plot_skymap_tract(skyMap, tract=0, title=None, ax=None,
+                      patch_colors=None):
     """
     Plot a tract from a skyMap.
 
@@ -106,6 +106,8 @@ def plot_skymap_tract(skyMap, tract=0, title=None, ax=None):
     ax: matplotlib.axes._subplots.AxesSubplot [None]
         The subplot object to contain the tract plot.  If None, then
         make a new one.
+    patch_colors: dict [None]
+        Dictionary of colors keyed by patchId.
 
     Returns
     -------
@@ -121,7 +123,7 @@ def plot_skymap_tract(skyMap, tract=0, title=None, ax=None):
     xNum, yNum = tract_info.getNumPatches()
 
     if ax is None:
-        fig = plt.figure(figsize=(12,8))
+        fig = plt.figure(figsize=(12, 8))
         ax = fig.add_subplot(111)
 
     tract_center = wcs.pixelToSky(tractBox.getCenter())\
@@ -134,11 +136,15 @@ def plot_skymap_tract(skyMap, tract=0, title=None, ax=None):
             patchBox = afw_geom.Box2D(patch_info.getOuterBBox())
             pixelPatchList = patchBox.getCorners()
             path = make_patch(pixelPatchList, wcs)
-            patch = patches.PathPatch(path, alpha=0.1, lw=1)
+            try:
+                color = patch_colors[(x, y)]
+            except (TypeError, KeyError):
+                color = 'blue'
+            patch = patches.PathPatch(path, alpha=0.1, lw=1, color=color)
             ax.add_patch(patch)
             center = wcs.pixelToSky(patchBox.getCenter())\
                         .getPosition(afw_geom.degrees)
-            ax.text(center[0], center[1], '%d,%d'%(x,y), size=6,
+            ax.text(center[0], center[1], '%d,%d' % (x, y), size=6,
                     ha="center", va="center")
 
     skyPosList = [wcs.pixelToSky(pos).getPosition(afw_geom.degrees)
@@ -147,7 +153,7 @@ def plot_skymap_tract(skyMap, tract=0, title=None, ax=None):
                 min(coord[0] for coord in skyPosList) - 1)
     ax.set_ylim(min(coord[1] for coord in skyPosList) - 1,
                 max(coord[1] for coord in skyPosList) + 1)
-    ax.grid(ls=':',color='gray')
+    ax.grid(ls=':', color='gray')
     ax.set_xlabel("RA (deg.)")
     ax.set_ylabel("Dec (deg.)")
     ax.set_title(title)
